23fd78aca3b221507a929210a1e9c79b783677a7
Tanguy Edelin <tedelin@Tanguys-Laptop.local>
2023-06-29 13:22:34 +0200

    ex02 done

--- ex02/MutantStack.hpp
+++ ex02/MutantStack.hpp
@@ -6,11 +6,41 @@
 /*   By: tedelin <tedelin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/26 16:57:17 by tedelin           #+#    #+#             */
-/*   Updated: 2023/06/26 16:57:31 by tedelin          ###   ########.fr       */
+/*   Updated: 2023/06/29 12:39:21 by tedelin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MUTANTSTACK_HPP
 # define MUTANTSTACK_HPP
+# include <stack>
+# include <deque>
+# include <iostream>
+
+template <typename T>
+class MutantStack : public std::stack<T>
+{
+    public:
+        MutantStack() {};
+        MutantStack(const MutantStack& cpy) {(void) cpy;};
+        MutantStack& operator=(const MutantStack& rhs) {(void) rhs;};
+        virtual ~MutantStack() {};
+
+        typedef typename std::deque<T>::iterator iterator;
+        typedef typename std::deque<T>::const_iterator const_iterator;
+        typedef typename std::deque<T>::reverse_iterator reverse_iterator;
+        typedef typename std::deque<T>::const_reverse_iterator const_reverse_iterator;
+
+        iterator begin() {return (std::stack<T>::c.begin());};
+        iterator end() {return (std::stack<T>::c.end());};
+
+        reverse_iterator rbegin() {return (std::stack<T>::c.rbegin());};
+        reverse_iterator rend() {return (std::stack<T>::c.rend());};
+
+        const_iterator  cbegin() {return (std::stack<T>::c.cbegin());};
+        const_iterator  cend() {return (std::stack<T>::c.cend());};
+
+        const_reverse_iterator crbegin() {return (std::stack<T>::c.begin());};
+        const_reverse_iterator crend() {return (std::stack<T>::c.end());};
+};
 
 #endif
--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,8 +6,56 @@
 /*   By: tedelin <tedelin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/26 16:57:08 by tedelin           #+#    #+#             */
-/*   Updated: 2023/06/26 16:57:14 by tedelin          ###   ########.fr       */
+/*   Updated: 2023/06/29 13:12:09 by tedelin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "MutantStack.hpp"
+#include <list>
+
+
+int main()
+{
+    {
+
+        MutantStack<int> mstack;
+        mstack.push(5);
+        mstack.push(17);
+        std::cout << mstack.top() << std::endl;
+        mstack.pop();
+        std::cout << mstack.size() << std::endl;
+        mstack.push(3);
+        mstack.push(5); 
+        mstack.push(737); //[...] 
+        mstack.push(0);
+        MutantStack<int>::iterator it = mstack.begin();
+        MutantStack<int>::iterator ite = mstack.end();
+        ++it;
+        --it;
+        while (it != ite) {
+            std::cout << *it << std::endl;
+        ++it; }
+        std::stack<int> s(mstack); 
+    }
+    {
+        std::cout << "---------------------" << std::endl;
+        std::list<int> mlist;
+        mlist.push_back(5);
+        mlist.push_back(17);
+        std::cout << mlist.back() << std::endl;
+        mlist.pop_back();
+        std::cout << mlist.size() << std::endl;
+        mlist.push_back(3);
+        mlist.push_back(5);
+        mlist.push_back(737);
+        mlist.push_back(0);
+        std::list<int>::iterator it = mlist.begin();
+        std::list<int>::iterator ite = mlist.end();
+        ++it;
+        --it;
+        while (it != ite) {
+            std::cout << *it << std::endl;
+        ++it; }
+        return 0;
+    }
+}
